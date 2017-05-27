f9e5e3460421e7975b4510255fe072ba66185e64
Udayan Sahai <vagrant@precise64.(none)>
2017-05-27 20:06:01 +0000

    Super block with csv

--- lab3a.c
+++ lab3a.c
@@ -10,30 +10,35 @@
 
 
 extern int errno; 
+int image_fd; 
+struct ext2_super_block superBlock; 
+void handleSuperBlock(); 
 
+void handleSuperBlock()
+{
+
+        int readSize = pread(image_fd, &superBlock , sizeof(superBlock) , 1024);  //read the size of the super block from the offset 1024.
+
+        if(readSize <= 0)
+                fprintf(stderr,"Read Failed: \n", strerror(errno));
+
+	        fprintf(stdout,"%s,%d,%d,%d,%d,%d,%d,%d\n","SUPERBLOCK",(int) superBlock.s_blocks_count
+		, (int) superBlock.s_inodes_count, 0, 0, (int) superBlock.s_blocks_per_group, (int) superBlock.s_inodes_per_group, 0 );
+
+}
 
 int main(int agrc, char ** argv)
 {
 
-	int fd = open(argv[1], O_WRONLY);
+	image_fd = open(argv[1], O_RDONLY);
 
-	if (fd < 0) 
+	if (image_fd < 0) 
 		fprintf(stderr,"Could not open file: \n", strerror(errno));
 
-	
-	struct  ext2_super_block superBlock;
 
-
-	int readSize = pread(fd, &superBlock , sizeof(superBlock) , 1024);  //read the size of the super block from the offset 1024.
-	
-	if(readSize <= 0) 
-		fprintf(stderr,"Read Failed: \n", strerror(errno));
-
-
-
-
-
-
-
+ 	handleSuperBlock();
 }
+
+
+
 
